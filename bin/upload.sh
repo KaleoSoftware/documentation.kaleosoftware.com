66ef1679273b80217d96a2140a4c5e733c21a775
John Lynch <john@rigelgroupllc.com>
2015-08-14 15:37:34 -0700

    Uploader notes

--- bin/upload.sh
+++ bin/upload.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-s3uploader -p true ~/code/kaleodocs documentation.kaleosoftware.com
+# Depends on gem install s3_uploader
+s3uploader -p true -k $AWS_ACCESS_KEY -s $AWS_SECRET_KEY ~/code/kaleodocs documentation.kaleosoftware.com
 
 
